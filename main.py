e0ac94d74f8ec76a4838d3d53132e517b7e2bd6e
RenattoGuzman <renttogs@gmail.com>
2025-09-10 15:37:59 -0600

    Main File to Print

---
+++ main.py
@@ -0,0 +1,7 @@
+from math_rg import square, factorial, is_prime, gcd, lcm
+
+print(square(5))       # 25
+print(factorial(5))    # 120
+print(is_prime(29))    # True
+print(gcd(24, 36))     # 12
+print(lcm(4, 6))       # 12
